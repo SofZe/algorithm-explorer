986bc5bc4d6c778f758638b8495f80bbb359e1e2
SofZe <sofi.dioli@mc-class.gr>
2026-05-24 11:51:36 +0300

    Update bubbleSort.js

--- src/algorithms/sorting/bubbleSort.js
+++ src/algorithms/sorting/bubbleSort.js
@@ -1,8 +1,12 @@
+// Function that creates all the steps of Bubble Sort
 export function generateBubbleSortSteps(inputArray) {
   const arr = [...inputArray];
+  // Stores all compare and swap actions
   const steps = [];
 
+  // Repeat passes through the array
   for (let i = 0; i < arr.length; i++) {
+    // Compare neighboring elements
     for (let j = 0; j < arr.length - i - 1; j++) {
 
       steps.push({
@@ -16,6 +20,7 @@ export function generateBubbleSortSteps(inputArray) {
         [arr[j], arr[j + 1]] =
           [arr[j + 1], arr[j]];
 
+        // Save the swap step
         steps.push({
           type: "swap",
           indices: [j, j + 1],
@@ -25,5 +30,6 @@ export function generateBubbleSortSteps(inputArray) {
     }
   }
 
+  // Return all Bubble Sort steps
   return steps;
 }
