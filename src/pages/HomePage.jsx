52d4d4d3c0caba78589f4a9afde31d9adcba4409
SofZe <sofi.dioli@mc-class.gr>
2026-05-24 12:04:19 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
+// Components
 function HomePage({ onEnter }) {
   return (
     <div className="home-page">
       <div className="overlay">
         <h1>Algorithm Explorer</h1>
 
+        // Description
         <p>
           Interactive Web-Based Algorithm Visualization System
         </p>
 
+        // button to enter the application
         <button onClick={onEnter}>
           Enter Explorer
         </button>
