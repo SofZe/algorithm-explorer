fdb070a4313ff06bf050835c822850835d2e8d99
SofZe <sofi.dioli@mc-class.gr>
2026-05-24 12:07:01 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,16 +1,23 @@
+// Imports
 import { useState } from "react";
 
+// Components
 function LoginPage({ onLogin }) {
+
+  // Stores the user name (locally only)
   const [name, setName] = useState("");
 
+  // Handle form submit
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    // Stops empty input
     if (name.trim() === "") return;
 
     onLogin(name);
   };
 
+  // JSX UI
   return (
     <div className="login-page">
       <form className="login-card" onSubmit={handleSubmit}>
